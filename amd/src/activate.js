a42b33377c0997eec86c00e39d6b165bfd203674
Pascal Collins <pascal.collins@iconnewmedia.de>
2023-01-24 14:14:34 +0100

    fixes a js issue that existed if one course had multiple instances

--- amd/src/activate.js
+++ amd/src/activate.js
@@ -1,6 +1,14 @@
+var mod_iconactivatecontent_initialized = false;
 define(['jquery'], function($){
     return {
         init: function() {
+            // prevent this init function from being called multiple times
+            // (can happen when there are multiple instances)
+            // that would add too many event listeners
+            if (mod_iconactivatecontent_initialized) {
+                return;
+            }
+            mod_iconactivatecontent_initialized = true;
             $('.iconactivatecontent:has(.isrestricted)').addClass('dimmed');
             $('.externalcontent button').on('click', function(){
                 var id = $(this).data('id');
